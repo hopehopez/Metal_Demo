8044e45eb1e7c547c7cd3956f56eb885f7528064
zhangshuqing <zhangshuqing912@163.com>
2020-08-20 18:31:48 +0800

    no message

--- Metal_Demo/001_Color/Render.swift
+++ Metal_Demo/001_Color/Render.swift
@@ -23,8 +23,10 @@ class Render: NSObject, MTKViewDelegate {
     static var growing = true
     static var primaryChannel = 0
     static var colorChannels = [1.0, 0.0, 0.0, 1.0]
-    
-    func initWithMetalKitView(mtkView: MTKView) {
+   
+    convenience init(mtkView: MTKView) {
+        self.init()
+        
         device = mtkView.device
         
         //Metal与GPU 交互的第一个对象
@@ -67,6 +69,37 @@ class Render: NSObject, MTKViewDelegate {
         
         //2. 设置view的clearColor
         view.clearColor = MTLClearColorMake(color.red, color.green, color.blue, color.alpha)
+        
+        //3. Create a new command buffer for each render pass to the current drawable
+        //使用MTLCommandQueue 创建对象并且加入到MTCommandBuffer对象中去.
+        //为当前渲染的每个渲染传递创建一个新的命令缓冲区
+        let commandBuffer = commandQueue?.makeCommandBuffer()
+        commandBuffer?.label = "MyCommand"
+        
+        //4.从视图绘制中,获得渲染描述符
+        //5.判断renderPassDescriptor 渲染描述符是否创建成功,否则则跳过任何渲染.
+        if let renderPassDescriptor = view.currentRenderPassDescriptor {
+            
+            //6.通过渲染描述符renderPassDescriptor创建MTLRenderCommandEncoder 对象
+            let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+            renderEncoder?.label = "renderEncoder"
+            
+            //7.我们可以使用MTLRenderCommandEncoder 来绘制对象,但是这个demo我们仅仅创建编码器就可以了,我们并没有让Metal去执行我们绘制的东西,这个时候表示我们的任务已经完成.
+            //即可结束MTLRenderCommandEncoder 工作
+            renderEncoder?.endEncoding()
+            
+            /*
+             当编码器结束之后,命令缓存区就会接受到2个命令.
+             1) present
+             2) commit
+             因为GPU是不会直接绘制到屏幕上,因此你不给出去指令.是不会有任何内容渲染到屏幕上.
+             */
+            //8.添加一个最后的命令来显示清除的可绘制的屏幕
+            commandBuffer?.present(view.currentDrawable!)
+        }
+        
+         //9.在这里完成渲染并将命令缓冲区提交给GPU
+        commandBuffer?.commit()
     }
     
 
--- Metal_Demo/001_Color/ViewController.swift
+++ Metal_Demo/001_Color/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 import MetalKit
 class ViewController: UIViewController {
-
+    var _render:Render!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //1. 获取MTKView
+        let view = self.view as! MTKView
+        
+        //2.为MTKView 设置MTLDevice(必须)
+        //一个MTLDevice对象就代表这着一个GPU, 通常我们可以调用方法MTLCreateSystemDefaultDevice() 来获取代表默认的GPU单个对象.
+        view.device = MTLCreateSystemDefaultDevice()
+        
+        //3.判断是否设置成功
+        if view.device == nil {
+            print("Metal is not supported on this device")
+            return
+        }
+        
+        //4. 创建Render
+        //分开你的渲染循环:
+        //在我们开发Metal 程序时, 将渲染循环分为自己创建的类, 是非常有用的一种方式, 使用单独的类, 我们可以更好管理初始化Metal, 以及Metal视图委托.
+        render = Render.init(mtkView: view)
+        
+        //5.判断render 是否创建成功
+//        if case let render == nil {
+//            print("Renderer failed initialization")
+//            return
+//        }
+        
+        //6.设置MTKView 的代理(由CCRender来实现MTKView 的代理方法)
+        view.delegate = render
+        
+        //7.视图可以根据视图属性上设置帧速率(指定时间来调用drawInMTKView方法--视图需要渲染时调用)
+        view.preferredFramesPerSecond = 60
+        
     }
 
 
