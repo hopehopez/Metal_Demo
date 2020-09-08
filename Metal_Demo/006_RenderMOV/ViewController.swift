60e23188540f018fee60e2fc6c2ce9cc6c52baf1
zhangshuqing <zhangshuqing912@163.com>
2020-09-08 18:45:19 +0800

    006 渲染视频文件

---
+++ Metal_Demo/006_RenderMOV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  006_RenderMOV
+//
+//  Created by zsq on 2020/9/8.
+//  Copyright © 2020 zsq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
